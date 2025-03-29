971dd8025a7401b55c978438f3f6529fe395ce40
gabrlcj <gabrielbittencourt57@gmail.com>
2025-03-29 11:11:34 -0300

    feat: adds components to dropzone and upload list

---
+++ src/components/upload-widget-dropzone.tsx
@@ -0,0 +1,5 @@
+export function UploadWidgetDropzone() {
+  return (
+    <div className="px-4 h-36"></div>
+  )
+}
---
+++ src/components/upload-widget-upload-list.tsx
@@ -0,0 +1,5 @@
+export function UploadWidgetUploadList() {
+  return (
+    <div className="px-4 h-44"></div>
+  )
+}
