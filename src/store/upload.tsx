33b6fbd6dabbe376c0cb036d1b407b8cfe0107fe
gabrlcj <gabrielbittencourt57@gmail.com>
2025-04-04 22:23:49 -0300

    feat: adds function to manage state to retry upload and to get pending uploads

--- src/store/upload.tsx
+++ src/store/upload.tsx
@@ -3,91 +3,107 @@ import { immer } from 'zustand/middleware/immer'
 import { enableMapSet } from 'immer'
 import { uploadFileToStorage } from '../http/upload-file-to-storage'
 import { CanceledError } from 'axios'
+import { useShallow } from 'zustand/shallow'
+import { compressImage } from '../utils/compress-image'
 
 export type Upload = {
   name: string
   file: File
-  abortController: AbortController
+  abortController?: AbortController
   status: 'progress' | 'success' | 'error' | 'canceled'
   originalSizeInBytes: number
   uploadSizeInBytes: number
+  compressedSizeInBytes?: number
+  remoteUrl?: string
 }
 
 type UploadState = {
   uploads: Map<string, Upload>,
   addUploads: (files: File[]) => void,
   cancelUpload: (uploadId: string) => void,
+  retryUpload: (uploadId: string) => void,
 }
 
 enableMapSet()
 
 export const useUploads = create<UploadState, [['zustand/immer', never]]>(
   immer((set, get) => {
+    function updateUpload(uploadId: string, data: Partial<Upload>) {
+      const upload = get().uploads.get(uploadId)
+
+      if (!upload) return
+
+      set(state => {
+        state.uploads.set(uploadId, { ...upload, ...data })
+      })
+    }
+
     async function processUpload(uploadId: string) {
       const upload = get().uploads.get(uploadId)
 
       if (!upload) return
 
+      const abortController = new AbortController()
+
+      updateUpload(uploadId, {
+        uploadSizeInBytes: 0,
+        compressedSizeInBytes: undefined,
+        remoteUrl: undefined,
+        status: 'progress',
+        abortController
+      })
+
       try {
-        await uploadFileToStorage(
+        const compressedFile = await compressImage({
+          file: upload.file,
+          maxHeight: 1000,
+          maxWidth: 1000,
+          quality: 0.8
+        })
+
+        updateUpload(uploadId, { compressedSizeInBytes: compressedFile.size })
+
+        const { url } = await uploadFileToStorage(
           {
-            file: upload.file,
+            file: compressedFile,
             onProgress(sizeInBytes) {
-              set(state => {
-                state.uploads.set(uploadId, {
-                  ...upload,
-                  uploadSizeInBytes: sizeInBytes,
-                })
-              })
+              updateUpload(uploadId, { uploadSizeInBytes: sizeInBytes })
             },
           },
-          { signal: upload.abortController.signal }
+          { signal: abortController.signal }
         )
-  
-        set(state => {
-          state.uploads.set(uploadId, {
-            ...upload,
-            status: 'success',
-          })
-        })
+
+        updateUpload(uploadId, { status: 'success', remoteUrl: url })
       } catch (error) {
         if (error instanceof CanceledError) {
-          set(state => {
-            state.uploads.set(uploadId, {
-              ...upload,
-              status: 'canceled',
-            })
-          })
+          updateUpload(uploadId, { status: 'canceled' })
 
           return
         }
 
-        set(state => {
-          state.uploads.set(uploadId, {
-            ...upload,
-            status: 'error',
-          })
-        })
+        updateUpload(uploadId, { status: 'error' })
       }
     }
 
-    async function cancelUpload(uploadId: string) {
+    function cancelUpload(uploadId: string) {
       const upload = get().uploads.get(uploadId)
 
       if (!upload) return
 
-      upload.abortController.abort()
+      upload.abortController?.abort()
+    }
+
+    function retryUpload(uploadId: string) {
+      processUpload(uploadId)
     }
 
     function addUploads(files: File[]) {
       for (const file of files) {
         const uploadId = crypto.randomUUID()
-        const abortController = new AbortController()
 
         const upload: Upload = {
           name: file.name,
           file,
-          abortController,
           status: 'progress',
           originalSizeInBytes: file.size,
           uploadSizeInBytes: 0
@@ -100,11 +116,44 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
         processUpload(uploadId)
       }
     }
-    
+
     return {
       uploads: new Map(),
       addUploads,
       cancelUpload,
+      retryUpload,
     }
   })
 )
+
+export const usePendingUploads = () => {
+  return useUploads(useShallow(store => {
+    const isThereAnyPendingUpload = Array
+      .from(store.uploads.values())
+      .some(upload => upload.status === 'progress')
+
+    if (!isThereAnyPendingUpload) {
+      return { isThereAnyPendingUpload, globalPercent: 100 }
+    }
+
+    const {
+      total,
+      uploaded
+    } = Array.from(store.uploads.values()).reduce((acc, upload) => {
+      if (upload.compressedSizeInBytes) {
+        acc.uploaded += upload.uploadSizeInBytes
+      }
+
+      acc.total += upload.compressedSizeInBytes ?? upload.originalSizeInBytes
+
+      return acc
+    }, { total: 0, uploaded: 0 })
+
+    const globalPercent = Math.min(
+      Math.round((uploaded * 100) / total),
+      100
+    )
+
+    return { isThereAnyPendingUpload, globalPercent }
+  }))
+}
